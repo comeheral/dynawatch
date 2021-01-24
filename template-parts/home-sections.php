cd644da1b4e3808d117ca73d2de10ee40e7954ad
Côme Héral <comeheral@gmail.com>
2021-01-24 20:31:53 +0100

    homepage background + features sections

--- template-parts/home-sections.php
+++ template-parts/home-sections.php
@@ -13,6 +13,7 @@
             $image = get_sub_field('home_sections_image');
             $order = get_sub_field('home_sections_order');
             $type = get_sub_field('home_sections_type');
+            $color = get_sub_field('home_sections_color');
             // Do something...
 
             /* echo '<p>';
@@ -31,17 +32,16 @@
             print_r($link);
             echo '</pre>'; */
 
-
             ?>
 
             <?php if($type === 'basic') : ?>
 
                 <div class="row home-section section-basic"  <?php if($order === 'right') : ?>style="flex-direction: row-reverse"<?php endif; ?>>
-                    <div class="column v-centered">
+                    <div class="half-col v-centered">
                         <img class="section-image" src="<?php echo $image['sizes']['large'] ?>" alt="<?php echo $image['alt'] ?>">
                     </div>
 
-                    <div class="column v-centered">
+                    <div class="half-col v-centered">
                         <h2 class="title -secondary"><?php echo $title; ?></h2>
                         <p class="text"><?php echo $description ?></p>
                         <a class="arrowlink" href="<?php echo $link['url'] ?>"><?php echo $link['title'] ?><?php get_template_part('icons/gradient-arrow') ?></a>
@@ -50,20 +50,45 @@
 
             <?php elseif($type === 'background') : ?>
 
-                <div class="row home-section section-background"  <?php if($order === 'right') : ?>style="flex-direction: row-reverse"<?php endif; ?>>
-                    <div class="column v-centered">
+                <div class="row home-section section-background"  style="background-color: <?php echo $color ?>; <?php if($order === 'right') : ?>flex-direction: row-reverse<?php endif; ?>">
+                    <div class="half-col v-centered">
                         <img class="section-image" src="<?php echo $image['sizes']['large'] ?>" alt="<?php echo $image['alt'] ?>">
                     </div>
 
-                    <div class="column v-centered">
-                        <div class="bg"></div>
-                        <h2 class="title -secondary"><?php echo $title; ?></h2>
-                        <p class="text"><?php echo $description ?></p>
-                        <a class="arrowlink" href="<?php echo $link['url'] ?>"><?php echo $link['title'] ?><?php get_template_part('icons/gradient-arrow') ?></a>
+                    <div class="half-col v-centered">
+                        <div class="background-content">
+                            <p class="text"><?php echo $description ?></p>
+                            <a class="arrowlink" href="<?php echo $link['url'] ?>"><?php echo $link['title'] ?><?php get_template_part('icons/gradient-arrow') ?></a>
+                        </div>
                     </div>
                 </div>
 
-            <?php endif; ?>
+            <?php elseif($type === 'features') : ?>
+
+                <div class="row home-section section-features">
+                    <?php
+                    if( have_rows('home_sections_features') ):
+                        while( have_rows('home_sections_features') ) : the_row();
+                        
+                        $features_title = get_sub_field('home_sections_features_title');
+                        $features_text = get_sub_field('home_sections_features_text');
+                        $features_icon = get_sub_field('home_sections_features_icon');
+                    ?>
+                    
+                    <div class="quarter-col features-item">
+                        <?php echo $features_icon ?>
+                        <h3 class="title -gradient"><?php echo $features_title ?></h3>
+                        <p class="text"><?php echo $features_text ?></p>
+                    </div>
+
+                    <?php
+                        endwhile;
+                    endif;
+                    ?>
+                
+                </div>
+
+            <?php endif ; ?>
 
 
             <?php
