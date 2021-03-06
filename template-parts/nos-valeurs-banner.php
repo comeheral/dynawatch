4ecae966ad952dc673938245bd0660571efecba6
Cha <charlene.villiette70@gmail.com>
2021-03-06 18:43:29 +0100

    Fin de la page nos valeurs + responsive

--- template-parts/nos-valeurs-banner.php
+++ template-parts/nos-valeurs-banner.php
@@ -17,10 +17,19 @@
 
 ?>
 
-<h1 class="banner-title title -primary"><?php echo $title ?></h1>
-<h2 class="banner-mini-subtitle  title -orange"><?php echo $minisubtitle ?></h2>
-<h3 class="banner-subtitle title -black"><?php echo $subtitle ?></h3>
-<p class="description text"><?php echo $description ?></p>
-<img class="banner-image" src="<?php echo $image['url'] ?>" alt="<?php echo $image['alt'] ?>">
+<div class="row nos-valeurs-banner">
+    <h1 class="banner-title title -primary" style="width:100%"><?php echo $title ?></h1>       
+    <div class="half-col v-centered banner">
+        <h2 class="banner-mini-subtitle title -orange"><?php echo $minisubtitle ?></h2>
+        <h3 class="banner-subtitle title -primary"><?php echo $subtitle ?></h3>
+        <p class="description text"><?php echo $description ?></p>
+    </div>
+    <div class="half-col v-centered">
+       <!--<h2 class="banner-mini-subtitle title -orange"><?php echo $minisubtitle ?></h2>
+        <h3 class="banner-subtitle title -black"><?php echo $subtitle ?></h3>
+        <p class="description text"><?php echo $description ?></p>-->
+    </div>
 
+</div>
 
+<img class="banner-blob blob-1" src="<?php echo($upload_dir); ?>/nos-valeurs-1.png" alt="Blob">
--- template-parts/nos-valeurs-sections.php
+++ template-parts/nos-valeurs-sections.php
@@ -1,5 +1,7 @@
 <?php
 
+$upload_dir = wp_get_upload_dir()["baseurl"] . "/" . $prefix /* . "-" */;
+
 // Check rows exists.
     if( have_rows('nos_valeurs_sections') ):
 
@@ -7,7 +9,7 @@
         while( have_rows('nos_valeurs_sections') ) : the_row();
 
             // Load sub field value.
-            $subtitle = get_sub_field('nos_valeurs_sections_subtitle'); //CHAMP ACF
+            $subtitle = get_sub_field('nos_valeurs_sections_subtitle');
             $title = get_sub_field('nos_valeurs_sections_title');
             $text = get_sub_field('nos_valeurs_sections_text');
             $image = get_sub_field('nos_valeurs_sections_images');
@@ -15,6 +17,9 @@
             $quoteText = get_sub_field('nos_valeurs_sections_quote_text');
             $quoteImgG = get_sub_field('nos_valeurs_sections_quote_img_g');
             $quoteImgD = get_sub_field('nos_valeurs_sections_quote_img_d');
+            $color = get_sub_field('nos_valeurs_sections_color');
+            $colorText = get_sub_field('nos_valeurs_color_text');
+
 
 
 
@@ -39,36 +44,47 @@
             ?>
 
             <?php if($type === 'basic') : ?>
-
+            <div class="section-background" style="background-color: <?php echo $color ?>">
                 <div class="row nos-valeurs-section section-basic">
+                    <div class="half-col v-centered img">
+                        <img class="section-image" src="<?php echo $image['sizes']['large'] ?>" alt="<?php echo $image['alt'] ?>">
+                    </div>
+
+                    <div class="half-col v-centered description">
+                        <h3 class="title -orange subtitle"><?php echo $subtitle ?></h3>
+                        <h2 class="title -secondary <?php if($colorText === 'blanc') : ?>-white<?php endif ?>"><?php echo $title; ?></h2>
+                        <p class="text <?php if($colorText === 'blanc') : ?>-white<?php endif ?>"><?php echo $text ?></p>
+                    </div>
+                </div>
+            </div>
+
+
+            <?php elseif($type === 'center') : ?>
+                <div class="row nos-valeurs-section section-center">
+                    <div class="centered-title">
+                        <h3 class="title -orange"><?php echo $subtitle ?></h3>
+                        <h2 class="title -secondary title-confidentiality"><?php echo $title; ?></h2>
+                    </div>
                     <div class="half-col v-centered">
                         <img class="section-image" src="<?php echo $image['sizes']['large'] ?>" alt="<?php echo $image['alt'] ?>">
                     </div>
 
-                    <div class="half-col v-centered">
-                        <h3 class="title -orange"><?php echo $subtitle ?></h3>
-                        <h2 class="title -secondary"><?php echo $title; ?></h2>
-                        <p class="text"><?php echo $description ?></p>
+                    <div class="half-col v-centered section-center-text">
                         <p class="text"><?php echo $text ?></p>
                     </div>
                 </div>
+                <div class="banner-blob-2">                
+                    <img class="blob-2" src="<?php echo($upload_dir); ?>/nos-valeurs-blob-2.png" alt="Blob">
+                </div>
+            </div>
+            
 
             <?php elseif($type === 'quotes') : ?>
 
                 <div class="row nos-valeurs-section section-quotes">
-                    <div class="">
-                        <img class="section-image" src="<?php echo $quoteImgG['sizes']['large'] ?>" alt="<?php echo $quoteImgG['alt'] ?>">
-                    </div>
-
-                    <div class="">
-                        <div class="background-content">
-                            <p class="text"><?php echo $quoteText ?></p>
-                        </div>
-                    </div>
-
-                    <div class="">
-                        <img class="section-image" src="<?php echo $quoteImgD['sizes']['large'] ?>" alt="<?php echo $quoteImgD['alt'] ?>">
-                    </div>
+                    <img class="section-image img-quote quote1" src="<?php echo $quoteImgG['sizes']['large'] ?>" alt="<?php echo $quoteImgG['alt'] ?>">
+                    <p class="text title -quote text-quote"><?php echo $quoteText ?></p>
+                    <img class="section-image img-quote quote2" src="<?php echo $quoteImgD['sizes']['large'] ?>" alt="<?php echo $quoteImgD['alt'] ?>">
                 </div>
 
             <?php endif ; ?>
