983e31b4cd35455e337961945f8ba2044eaac689
Côme Héral <comeheral@gmail.com>
2021-03-09 21:44:11 +0100

    html 404

--- 404.php
+++ 404.php
@@ -1,5 +1,8 @@
 <?php get_header() ?>
-
-    <h1>404</h1>
+    <div class="error404">
+        <h1 class="error404__title">404</h1>
+        <p class="error404__text">Page introuvable.</p>
+        <a class="error404__link" href="<?php bloginfo("url"); ?>">Retourner à la page d'accueil</a>
+    </div>
 
 <?php get_footer() ?>
