bcf75c9e6e8fea97206390282a4574f9f5a519f6
Cha <charlene.villiette70@gmail.com>
2021-03-10 17:07:44 +0100

    Dernières modifs nos valeurs CSS

--- template-parts/nos-valeurs-banner.php
+++ template-parts/nos-valeurs-banner.php
@@ -17,19 +17,18 @@
 
 ?>
 
-<div class="row nos-valeurs-banner">
-    <h1 class="banner-title title -primary" style="width:100%"><?php echo $title ?></h1>       
-    <div class="half-col v-centered banner">
-        <h2 class="banner-mini-subtitle title -orange"><?php echo $minisubtitle ?></h2>
-        <h3 class="banner-subtitle title -primary"><?php echo $subtitle ?></h3>
-        <p class="description text"><?php echo $description ?></p>
-    </div>
-    <div class="half-col v-centered">
-       <!--<h2 class="banner-mini-subtitle title -orange"><?php echo $minisubtitle ?></h2>
-        <h3 class="banner-subtitle title -black"><?php echo $subtitle ?></h3>
-        <p class="description text"><?php echo $description ?></p>-->
+<div class="row">
+    <h1 class="banner-title title -primary" style="width:100%"><?php echo $title ?></h1>    
+    <div class="div-valeurs-banner">   
+        <div class="half-col v-centered banner-txt">
+            <h2 class="banner-mini-subtitle title -orange"><?php echo $minisubtitle ?></h2>
+            <h3 class="banner-subtitle title -primary"><?php echo $subtitle ?></h3>
+            <p class="description text"><?php echo $description ?></p>
+        </div>
+        <div class="half-col v-centered banner-img">
+            <img class="banner-blob blob-1" src="<?php echo($upload_dir); ?>/nos-valeurs-1.png" alt="Blob">
+        </div>
     </div>
 
 </div>
 
-<img class="banner-blob blob-1" src="<?php echo($upload_dir); ?>/nos-valeurs-1.png" alt="Blob">
